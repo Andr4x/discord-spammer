3fa75b422d825d36bc73c11fec6c0a3ee56a0034
Andr4x <58180125+Andr4x@users.noreply.github.com>
2020-01-07 03:10:53 -0300

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,23 @@
+#Coded By Andr4x From OffensiveEncode Org.
+import discord
+import asyncio
+import sys
+
+                                                  
+print("Usage: python spammer.py token channel text")
+
+client = discord.Client()
+token = sys.argv[1]
+DiscordChannel = sys.argv[2]
+spam_text = sys.argv[3]
+
+@client.event
+async def on_message(message):
+    if message.content == "start":
+        while True:
+            await message.channel.send(spam_text)
+    if message.content == "stop":
+        await client.close()
+        sys.exit()
+
+client.run(token, bot=False)
